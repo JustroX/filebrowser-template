71dfd495c0ac1d141c32233b36ff11735fed757c
JustroX <thepiesaresquared@gmail.com>
2024-02-21 06:41:52 +0800

    feat: specify database

--- start.sh
+++ start.sh
@@ -55,8 +55,8 @@ else
 
     echo setting configurations
     /filebrowser config set --address "0.0.0.0" --database $DATABASE_PATH > /dev/null
-    /filebrowser config set --commands zip,unzip,ls,bash > /dev/null
-    /filebrowser config set --shell 'bash -c' > /dev/null
+    /filebrowser config set --commands zip,unzip,ls,bash --database $DATABASE_PATH > /dev/null
+    /filebrowser config set --shell 'bash -c' --database $DATABASE_PATH > /dev/null
 
     /filebrowser users add $WEB_USERNAME $WEB_PASSWORD --database $DATABASE_PATH > /dev/null
 fi
